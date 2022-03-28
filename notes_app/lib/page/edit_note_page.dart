5eb3285ee2c01c4f5321e9eca1c0721c435d3b8d
matwork4 <matwork4@gmail.com>
2022-03-28 21:53:26 +0200

    SQFlite CRUD pour le journal
    Dans un nouveau projet, ajout des méthodes Create, Read, Update et Delete pour la database du journal avec SQFlite.
    L'implémentation UI n'est pas terminée.
    Utilisation de Nullsafety (version 2.12 de flutter)

---
+++ notes_app/lib/page/edit_note_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/db/notes_database.dart';
+import 'package:notes_app/model/note.dart';
+/*import 'package:notes_app/widget/note_form_widget.dart';*/
+
+class AddEditNotePage extends StatefulWidget {
+  final Note? note;
+
+  const AddEditNotePage({
+    Key? key,
+    this.note,
+  }) : super(key: key);
+  @override
+  _AddEditNotePageState createState() => _AddEditNotePageState();
+}
+
+class _AddEditNotePageState extends State<AddEditNotePage> {
+  final _formKey = GlobalKey<FormState>();
+  late bool isImportant;
+  late int number;
+  late String title;
+  late String description;
+
+  // /!\ Il manque cette partie
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+  void addOrUpdateNote() async {
+    final isValid = _formKey.currentState!.validate();
+
+    if (isValid) {
+      final isUpdating = widget.note != null;
+
+      if (isUpdating) {
+        await updateNote();
+      } else {
+        await addNote();
+      }
+      Navigator.of(context).pop();
+    }
+  }
+
+  Future updateNote() async {
+    final note = widget.note!.copy(
+      isImportant: isImportant,
+      number: number,
+      title: title,
+      description: description,
+    );
+
+    await NotesDatabase.instance.update(note);
+  }
+
+  Future addNote() async {
+    final note = Note(
+      title: title,
+      isImportant: true,
+      number: number,
+      description: description,
+      createdTime: DateTime.now(),
+    );
+
+    await NotesDatabase.instance.create(note);
+  }
+}
