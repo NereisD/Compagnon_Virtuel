747a21735786c7111903ad7a15976872a606f8c5
matwork4 <matwork4@gmail.com>
2022-05-11 20:34:10 +0200

    GroupBy date
    GroupedListView pour grouper les messages par date

--- compagnon/lib/screens/chat/components/chatBox.dart
+++ compagnon/lib/screens/chat/components/chatBox.dart
@@ -5,6 +5,7 @@ import 'package:compagnon/db/MessageDatabase.dart';
 import 'package:compagnon/providers/todos.dart';
 import 'package:compagnon/screens/chat/chat_body.dart';
 import 'package:flutter/material.dart';
+import 'package:grouped_list/grouped_list.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
@@ -28,10 +29,43 @@ class MyChatBox extends State<ChatBox> {
         builder: (BuildContext context, AsyncSnapshot<List<Message>> snapshot) {
           if (snapshot.hasData) {
             List<Message> messages = snapshot.data;
-            return ListView.builder(
-              itemCount: messages.length,
-              itemBuilder: (context, index) => MessagePlacement(
-                message: messages[index],
+            return GroupedListView<Message, DateTime>(
+              padding: const EdgeInsets.all(8),
+              reverse: true,
+              order: GroupedListOrder.DESC,
+              useStickyGroupSeparators: true,
+              floatingHeader: true,
+              elements: messages, // La liste des messages va ici
+              groupBy: (message) => DateTime(
+                //On regroupe les messages d'un même jour ensemble
+                message.date.year,
+                message.date.month,
+                message.date.day,
+              ),
+              /* Un header pour chaque groupe de message 
+              * (un groupe étant un jour)
+              * C'est un Card widget avec la date du jour
+              */
+              groupHeaderBuilder: (Message message) => SizedBox(
+                height: 40,
+                child: Center(
+                  child: Card(
+                    color: Theme.of(context).secondaryHeaderColor,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Text(
+                        //Nécessite la librairie intl
+                        DateFormat.yMMMd().format(message.date),
+                        //Style du widget date (le fond bleu)
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              //itemCount: messages.length,
+              itemBuilder: (context, Message message) => MessagePlacement(
+                message: message,
                 key: null,
               ),
             );
