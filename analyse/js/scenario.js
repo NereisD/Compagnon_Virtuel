304b36a7fb6ed990aa8372168a3317bdeee97199
matwork4 <51155034+matwork4@users.noreply.github.com>
2022-08-04 16:50:28 +0900

    New & edit reply DOM

--- analyse/js/scenario.js
+++ analyse/js/scenario.js
@@ -34,7 +34,7 @@ function findQuestion(id){
 			return list_questions[i];
 		}
 	}
-	print("Error findQuestion("+id+") not found");
+	console.log("Error findQuestion("+id+") not found");
 	return 1;
 }
 
@@ -47,7 +47,7 @@ function findReply(id){
 			return list_replies[i];
 		}
 	}
-	print("Error findReply("+id+") not found");
+	console.log("Error findReply("+id+") not found");
 	return 1;
 }
 
@@ -161,6 +161,7 @@ function deleteQuestion(id){
 	for(var i=0; i<list_replies.length; i++){
 		if(list_replies[i].idQuestion == id){
 			list_replies.splice(i,1);
+			i=i-1;
 		}
 	}
 
@@ -276,7 +277,8 @@ function returnLastWord(text){
 }
 
 
-/* Récupère l'id reply et vérifie si il est déjà existant
+/* Vérifie si l'id question existe
+ * Récupère l'id reply et vérifie si il est déjà existant
  * si oui : modifie
  * si non : créer
  */
@@ -285,6 +287,17 @@ function validateReply(){
 	//On récupère l'id dans le DOM
 	var edit_reply_id = document.getElementById("edit_reply_id");
 	var id = parseInt(returnLastWord(edit_reply_id.innerText));
+
+	//On vérifie si l'ID Question est valide
+	var edit_reply_previous = document.getElementById("edit_reply_previous");
+	var idQuestion = parseInt(edit_reply_previous.value);
+
+	//Renvoie une erreur si ID previous question n'est pas trouvé
+	if(findQuestion(idQuestion) == 1){
+		console.log("Error : id Previous question is not valid");
+		alert("ID Previous question is not valid")
+		return 1;
+	}
 
 	//On vérifie si il existe ou non
 	var exist = false;
@@ -296,12 +309,228 @@ function validateReply(){
 
 	if(exist){
 		//Fonction modifie
+		modifyReply(id);
 	}else{
 		//Fonction créer 
+		createReply(id);
 	}
+
+	//On ferme la fenètre
+	displayEdit_reply(false);
 
 }
 
+/* Modifie le DOM d'une Reply
+ * La déplace en dessous de la bonne question - a faire
+ */
+function modifyReply(id){
+
+	//On récupère l'objet Reply dans la liste
+	var r = findReply(id);
+
+	//-----------------------------------
+	//On modifie l'objet Reply
+
+	var edit_reply_previous = document.getElementById("edit_reply_previous");
+	r.idQuestion=parseInt(edit_reply_previous.value);
+
+	var edit_reply_next = document.getElementById("edit_reply_next");
+	r.idNextQuestion=parseInt(edit_reply_next.value);
+
+	var edit_reply_type = document.getElementById("edit_reply_type");
+	r.dataType=edit_reply_type.value;
+
+	var edit_reply_value = document.getElementById("edit_reply_value");
+	r.dataValue=parseInt(edit_reply_value.value);
+
+	var edit_reply_textarea = document.querySelector("[id='edit_reply_textarea']");
+	r.textEN=edit_reply_textarea.children[0].value;
+	r.textFR=edit_reply_textarea.children[1].value;
+	r.textJP=edit_reply_textarea.children[2].value;
+	//-----------------------------------
+
+	//On modifie le DOM Reply
+	var elementReply = document.querySelector("[idReply='"+id+"']");
+
+	//console.log(elementReply.children[1].children[0].innerText);
+	elementReply.children[1].children[0].innerText = "Previous : "+r.idQuestion;
+	elementReply.children[1].children[1].innerText = "Next : "+r.idNextQuestion;
+
+	elementReply.children[2].children[0].innerText = "Type : "+r.dataType;
+	elementReply.children[2].children[1].innerText = "Value : "+r.dataValue;
+
+	elementReply.children[3].innerText = r.textEN;
+	elementReply.children[4].innerText = r.textFR;
+	elementReply.children[5].innerText = r.textJP;
+
+	//Déplace la réponse en DOM
+	replaceReply(elementReply,id);
+
+}
+
+
+/* Deplace une réponse DOM sous la bonne question
+ */
+function replaceReply(elementReply, id){
+	var r = findReply(id);
+
+	var elementZone = document.querySelector("[idZone='"+r.idQuestion+"']");
+	elementZone.appendChild(elementReply);
+}
+
+
+/* Créer une réponse en objet et en DOM
+ * La place en dessous d'une question 
+ */
+function createReply(id){
+
+	//-------------------
+	//Création en objet
+	var edit_reply_previous = document.getElementById("edit_reply_previous");
+	var idQuestion = parseInt(edit_reply_previous.value);
+
+	var edit_reply_next = document.getElementById("edit_reply_next");
+	var idNextQuestion=parseInt(edit_reply_next.value);
+	
+	//id, isScenario, previous, next
+	var r = new Reply(id,ID_SCENARIO,idQuestion,idNextQuestion);
+
+	var edit_reply_type = document.getElementById("edit_reply_type");
+	r.dataType=edit_reply_type.value;
+
+	var edit_reply_value = document.getElementById("edit_reply_value");
+	r.dataValue=parseInt(edit_reply_value.value);
+
+	var edit_reply_textarea = document.querySelector("[id='edit_reply_textarea']");
+	r.textEN=edit_reply_textarea.children[0].value;
+	r.textFR=edit_reply_textarea.children[1].value;
+	r.textJP=edit_reply_textarea.children[2].value;
+
+	//On ajoute la réponse dans la liste
+	list_replies.push(r);
+	//-------------------
+
+	//-------------------
+	//Création en DOM
+
+	var elementReply = document.createElement("div");
+	elementReply.setAttribute('class','reply');
+	elementReply.setAttribute('idReply',r.id);
+
+	var elementBtn_reply = document.createElement("div");
+	elementBtn_reply.setAttribute('id','btn_reply');
+
+	var elementP1 = document.createElement("p");
+	elementP1.innerText = "Id reply : "+r.id;
+
+	var elementBtn1 = document.createElement("button");
+	elementBtn1.innerText = "Edit";
+	elementBtn1.setAttribute('onclick','editEdit_reply('+r.id+')');
+
+	var elementBtn2 = document.createElement("button");
+	elementBtn2.innerText = "X";
+	elementBtn2.setAttribute('onclick','deleteReply('+r.id+')');
+	elementBtn2.setAttribute('id','btn_delete');
+
+	elementBtn_reply.appendChild(elementP1);
+	elementBtn_reply.appendChild(elementBtn1);
+	elementBtn_reply.appendChild(elementBtn2);
+
+	elementReply.appendChild(elementBtn_reply);
+
+	var elementDouble1 = document.createElement("div");
+	elementDouble1.setAttribute('class','double');
+
+	var elementP2 = document.createElement("p");
+	elementP2.innerText = "Previous : "+r.idQuestion;
+
+	var elementP3 = document.createElement("p");
+	elementP3.innerText = "Next : "+r.idNextQuestion;
+
+	elementDouble1.appendChild(elementP2);
+	elementDouble1.appendChild(elementP3);
+
+	elementReply.appendChild(elementDouble1);
+
+	var elementDouble2 = document.createElement("div");
+	elementDouble2.setAttribute('class','double');
+
+	var elementP4 = document.createElement("p");
+	elementP4.innerText = "Type : "+r.dataType;
+
+	var elementP5 = document.createElement("p");
+	elementP5.innerText = "Value : "+r.dataValue;
+
+	elementDouble2.appendChild(elementP4);
+	elementDouble2.appendChild(elementP5);
+
+	elementReply.appendChild(elementDouble2);
+
+	var elementEN = document.createElement("p");
+	elementEN.innerText = r.textEN;
+	elementEN.setAttribute('id','textEN');
+
+	var elementFR = document.createElement("p");
+	elementFR.innerText = r.textFR;
+	elementFR.setAttribute('id','textFR');
+
+	var elementJP = document.createElement("p");
+	elementJP.innerText = r.textJP;
+	elementJP.setAttribute('id','textJP');
+
+	elementReply.appendChild(elementEN);
+	elementReply.appendChild(elementFR);
+	elementReply.appendChild(elementJP);
+
+	//ajoute elementReply au DOM
+	replaceReply(elementReply,r.id);
+
+	//Permet de reset la langue et ainsi cacher
+	//les réponses des autres langues
+	changeLanguage(lang);
+
+}
+
+/* Creer une question en DOM et en Objet
+ * avec id incrémenté 
+ */
+function createQuestion(){
+
+	//On trouve l'id de la prochaine question 
+	var id = ID_SCENARIO*10;
+	for(var i=0; i<list_questions.length; i++){
+		if(list_questions[i].id >= id){
+			id = list_questions[i].id + 1;
+		}
+	}
+
+	//Creer la question en Objet
+	var q = new Question(id,ID_SCENARIO);
+	list_questions.push(q);
+
+	//-----------------------------------
+	//Creer la question en DOM
+
+	var elementZone = document.createElement("div");
+	elementZone.setAttribute('class','zone_question');
+	elementZone.setAttribute('idZone',q.id);
+
+	var elementQuestion = document.createElement("div");
+	elementQuestion.setAttribute('class','question');
+	elementQuestion.setAttribute('id',q.id);
+
+	/* ---------------------------
+	 * ICI on va créer la question
+     * ---------------------------
+     */
+
+	//On créer l'élément 
+	elementZone.appendChild(elementQuestion);
+	document.getElementById("scenario_box").appendChild(elementZone);
+	
+	//-----------------------------------
+
+}
 
 
 
@@ -314,10 +543,10 @@ function validateReply(){
  * Fonction Edit Reply V
  * Fonction Delete Reply V
  * Fonction Validate Reply : V
- *  - Si déja existant, modifie
- *  - Sinon créer 
- * Fonction qui créer une Reply en DOM
- * Fonction qui modifie une Reply en DOM
+ *  - Si déja existant, modifie V
+ *  - Sinon créer V 
+ * Fonction qui créer une Reply en DOM V
+ * Fonction qui modifie une Reply en DOM V
  * 
  * implémenter les Replies - pop up new reply V
  * possibilité de mettre les replies en dessous des questions V
